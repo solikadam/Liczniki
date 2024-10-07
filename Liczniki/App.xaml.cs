5c7539fc9c1a2c7b0d3eab049edc8237c43658df
Adam Solipiwko <solpiw14@gmail.com>
2024-10-07 18:46:13 +0200

    Dodaj pliki projektów.

---
+++ Liczniki/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Liczniki
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
